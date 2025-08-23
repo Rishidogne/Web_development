ab36ee99f552cd4b3c0d8294f827c1ea2b5cef5f
Rishidogne <rishidogne2004@gmail.com>
2025-08-23 21:53:51 +0530

    Resolved merge conflicts

---
+++ lecture20/script.js
@@ -0,0 +1,35 @@
+// var name="rishi";
+// var name="amit";
+// var name="pooja";
+
+// let name="rishi";
+// let name="amit";
+
+// const x=5;
+
+// global scope
+// let y=20;
+
+// {
+//     //local scope 
+//     console.log(y);
+//     let x=10;
+//     console.log(x);
+// }
+//     console.log(y);
+
+
+// {
+//     let x=15;
+//     console.log(x);
+// }
+
+
+let name="rishi";
+let age=20;
+let student=true;
+let a;
+let b=null;
+let p=BigInt("1232345869495");
+let q=Symbol("hello!");
+
