1476a2d5fd8f268624bd1bce4370220a5f27be83
Rishidogne <rishidogne2004@gmail.com>
2025-08-30 22:24:07 +0530

    Resolved merge conflicts

---
+++ lecture22/loop.js
@@ -0,0 +1,88 @@
+
+// let n=5;
+// do{
+//     console.log("hello");
+//     n++;
+// }while(n<9);
+
+//pooja=4
+//prince=4
+//radhika=4
+
+//1 question
+  for(let i = 1; i <= 10; i++) {
+        console.log( "7 x " , i ," = " , 7*i );
+    }
+ //2 question
+    for(let i = 1; i <= 10; i++) {
+        console.log( "Square of " , i , " = " , i*i)
+    }
+
+// 3 question
+    let count = 0;
+    for(let i = 1; i <= 50; i++) {
+        if(i % 5 == 0) count++;
+    }
+    console.log("Numbers divisible by 5 between 1 and 50 = " , count)
+    
+    // 4 question
+    for(let i = 10; i >= 1; i--) {
+        cout << i << " ";
+    }
+   
+
+//5 question
+    let count = 0;
+    i = 1;
+    while(i <= 100) {
+        if(i % 3 == 0) count++;
+        i++;
+    }
+    console.log( "Count divisible by 3 = ", count);
+// 6 question
+    let fact = 1, n = 6;
+    i = 1;
+    while(i <= n) {
+        fact *= i;
+        i++;
+    }
+    console.log( "Factorial of 6 = " ,fact);
+
+
+
+// 7 question
+let fact3 = 1;
+n = 5;
+i = 1;
+do {
+  fact3 *= i;
+  i++;
+} while (i <= n);
+console.log("Factorial of 5 =", fact3);
+
+// 8 question
+let sum3 = 0;
+i = 1;
+do {
+  sum3 += i;
+  i++;
+} while (i <= 50);
+console.log("Sum =", sum3);
+
+// 9 question
+let count3 = 0;
+i = 1;
+do {
+  if (i % 7 === 0) count3++;
+  i++;
+} while (i <= 100);
+console.log("Divisible by 7 count =", count3);
+
+
+//10 question
+  i = 1;
+    do {
+        console.log(i , " ");
+        i += 2;
+    } while(i <= 15);
+    cout << endl;
