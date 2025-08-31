7ae68a7c85ff3dddfe1de2d996dd2b8e50979406
Rishidogne <rishidogne2004@gmail.com>
2025-08-31 22:14:21 +0530

    Resolved merge conflicts

---
+++ lecture23/string.js
@@ -0,0 +1,49 @@
+
+//guessing the correct gamenumber in the game.
+
+// let gamenumber=25;
+// let usernumber = prompt("Guess the game number : ");//15
+
+
+// while(gamenumber!=usernumber){
+//   usernumber=prompt("you entered number is not correct,So guess again: ");
+// }
+// console.log("congratulation ,you enter the correct value");
+
+//1 question
+  n = 1234;
+    let rev = 0;
+    while(n > 0) {
+        let d = n % 10;
+        rev = rev * 10 + d;
+        n /= 10;
+    }
+    console.log("Reversed number = ",rev );
+// 2 question
+       let prod = 1;
+    for(let i = 1; i <= 9; i += 2) {
+       prod *= i
+    }
+    console.log(prod);
+
+    //3 question
+    let sum = 0;
+    for(let i = 2; i <= 50; i += 2) {
+        sum += i;
+    }
+    console.log(sum);
+
+    //4 question
+    let n = 12345;
+    let count = 0;
+    while(n > 0) {
+        count++;
+        n /= 10;
+    }
+    console.log("Digits count = " , count);
+
+// 5. Print first 10 multiples of 3
+// 6. Print first 10 natural numbers in reverse order
+// 7.Print the sum of squares of the first 10 natural numbers.
+//8.Print the cube of numbers from 1 to 20.
+//9.Write a program to print all numbers divisible by both 3 and 7 between 1 and 200.
