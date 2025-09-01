aa6824772d539bc95e39c25e7461157874535736
Rishidogne <rishidogne2004@gmail.com>
2025-09-01 22:15:31 +0530

    Resolved merge conflicts

--- lecture23/string.js
+++ lecture23/string.js
@@ -11,39 +11,54 @@
 // console.log("congratulation ,you enter the correct value");
 
 //1 question
-  n = 1234;
-    let rev = 0;
-    while(n > 0) {
-        let d = n % 10;
-        rev = rev * 10 + d;
-        n /= 10;
-    }
-    console.log("Reversed number = ",rev );
-// 2 question
-       let prod = 1;
-    for(let i = 1; i <= 9; i += 2) {
-       prod *= i
-    }
-    console.log(prod);
+//   n = 1234;
+//     let rev = 0;
+//     while(n > 0) {
+//         let d = n % 10;
+//         rev = rev * 10 + d;
+//         n /= 10;
+//     }
+//     console.log("Reversed number = ",rev );
 
-    //3 question
-    let sum = 0;
-    for(let i = 2; i <= 50; i += 2) {
-        sum += i;
-    }
-    console.log(sum);
+   // 2 question
+    //    let prod = 1;
+    // for(let i = 1; i <= 9; i += 2) {
+    //    prod *= i
+    // }
+    // console.log(prod);
 
-    //4 question
-    let n = 12345;
-    let count = 0;
-    while(n > 0) {
-        count++;
-        n /= 10;
-    }
-    console.log("Digits count = " , count);
+    // //3 question
+    // let sum = 0;
+    // for(let i = 2; i <= 50; i += 2) {
+    //     sum += i;
+    // }
+    // console.log(sum);
+
+    // //4 question
+    // let n = 12345;
+    // let count = 0;
+    // while(n > 0) {
+    //     count++;
+    //     n /= 10;
+    // }
+    // console.log("Digits count = " , count);//5
+
 
 // 5. Print first 10 multiples of 3
 // 6. Print first 10 natural numbers in reverse order
 // 7.Print the sum of squares of the first 10 natural numbers.
+// let sum=0;
+// for(let i=1; i<=10; i++){
+//     sum+=i;
+// }
+// console.log(sum*sum);
+
+
 //8.Print the cube of numbers from 1 to 20.
 //9.Write a program to print all numbers divisible by both 3 and 7 between 1 and 200.
+
+// for(let i=1; i<=200; i++){
+//     if(i%3==0 && i%7==0){
+//         console.log(i,"is divided by both the 3 and 7 ");
+//     }
+// }
---
+++ lecture24/string.js
@@ -0,0 +1,30 @@
+// let str="rishi";
+// let age=20;
+// console.log("The age of ",str," is ",age);
+//length
+// let len=str.length;
+//indicies
+ //console.log(str[0]);
+
+//Template Literals
+// let para=`The age of ${str} is ${age}`;
+// console.log(para); 
+
+ let obj={
+   item:"pen",
+   price:10,
+ };
+
+
+//console.log("the item is",obj.item,"those price is",obj.price);
+//placeholder=${obj.item}
+//ecapes key words= \n,\t
+// let para=`The ITem is ${obj.item} Those Price Is ${obj.price}`;
+// let para2=`those price is ${obj.price}`;
+//let lower=para.toLowerCase();
+//let upper=para.toUpperCase();
+// let para=" rishi ";
+//  para=para.trim();
+// console.log(para.length);
+// console.log(para2);
+
