fe4d8a7ed264242e098769ccd75c66ffdd1f93dc
Rishidogne <rishidogne2004@gmail.com>
2025-09-07 21:16:55 +0530

    Resolved merge conflicts

--- lecture28/button.js
+++ lecture28/button.js
@@ -1,7 +1,33 @@
-let inputBtn=document.querySelector('button');
-inputBtn.addEventListener('click',inputMsg);
+// let inputBtn=document.querySelector('button');
+// inputBtn.addEventListener('click',inputMsg);
 
-function inputMsg(){
-    let name=prompt("Enter the you name :");
-    inputBtn.textContent='Click :By'+name;
+// function inputMsg(){
+//     let name=prompt("Enter the you name :");
+//     inputBtn.textContent='Click :By'+name;
+// }
+
+
+// Simple calculator using switch case
+// 1-5
+let a = Number( prompt("Enter the value of A:-") );
+let b = Number( prompt("Enter the value of B:-") );
+let op =prompt("enter the operator :-");
+
+switch (op) {
+    case "+":
+        console.log(a+b); 
+        break;
+    case "-":
+        console.log(a-b);
+        break;
+    default :
+        console.log(value,"is the invalid case");
 }
+
+
+
+
+////DOM selsctor
+//console.log(document.documentElement);
+
+// console.log(document.body.lastElementChild);
